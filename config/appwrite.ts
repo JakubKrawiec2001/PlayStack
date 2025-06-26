ed6ab3a1bb0afbf9f22193f4733e9d585930b8b0
Jakub Krawiec <kubakrawieckk04@gmail.com>
2025-06-26 21:13:51 +0200

    Add appwrite config

---
+++ config/appwrite.ts
@@ -0,0 +1,17 @@
+import { Client, Account } from "appwrite";
+
+export const client = new Client();
+
+const APPWRITE_PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
+const APPWRITE_ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+
+if (!APPWRITE_PROJECT_ID || !APPWRITE_ENDPOINT) {
+  throw new Error(
+    "Appwrite project ID and endpoint must be set in environment variables."
+  );
+}
+
+client.setEndpoint(APPWRITE_ENDPOINT).setProject(APPWRITE_PROJECT_ID);
+
+export const account = new Account(client);
+export { ID } from "appwrite";
