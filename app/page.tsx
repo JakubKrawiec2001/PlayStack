13898b1ad4df721c7457e4556080eadd578e9cae
Jakub Krawiec <kubakrawieckk04@gmail.com>
2025-06-27 21:00:16 +0200

    Add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <h1>PlayStack</h1>
     </div>
   );
 }
